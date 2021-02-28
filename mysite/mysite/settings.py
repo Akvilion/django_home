9b8afc230f31922a247c563e3fdaa80543237777
Akvilion <ianko.andrii@gmail.com>
2021-02-28 13:05:20 +0200

    install DB drivers

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0ltx_=wccu6+++7x9o&8x6l$e^rpvnn2!-@*a=@yk0h4*webex'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
